17e6b8b467baaeb37adcac686c70aa4967616d9a
kirill <kiruha3@mail.ru>
2023-08-04 18:04:22 +0700

    1 commit

---
+++ src/hogwarts.java
@@ -0,0 +1,12 @@
+import java.security.PrivateKey;
+
+public class hogwarts {
+    private String name;
+    private String surname;
+    private int power;
+    private int transgressiya;
+
+    public class hogwarets(String name, String surname, int power, int transgressiya) {
+        this.name = name;
+    }
+}
